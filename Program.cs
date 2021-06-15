f28da394eb9936858a7f9bb860ed99f0fea73bdb
Tapiwa Gwede <gwedet@usf.edu>
2021-06-14 20:32:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            int x;
+            int y;
+
+            x = 36;
+            y = 4;
+            int z = x + y;
+
+            Console.WriteLine("You are: " + z + " " + "years this year");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
